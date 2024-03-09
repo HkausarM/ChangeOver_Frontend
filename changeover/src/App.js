4a9413ddae88cedf8463fe7576d7f18ffd1e253f
HennaKausar <hennakausarformal@gmail.com>
2024-03-09 22:27:24 +0000

    adding sign up and login comp

--- changeover/src/App.js
+++ changeover/src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
-// import HomePage from './components/Pages/Home';
-import LoginPage from '../src/components/Login/Login'
+import Login from '../src/components/Login/Login'
+import Signup from './components/Login/SignIn';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <div className='homeLayout'>
-          <LoginPage></LoginPage>
-          {/* <HomePage className='sidenav' /> */}
+          <Router>
+            <Routes>
+              <Route  path="/" element={<Login/>} />
+              <Route  path="/signup" element={<Signup/>} />
+            </Routes>
+          </Router>
         </div>
       </header>
     </div>
